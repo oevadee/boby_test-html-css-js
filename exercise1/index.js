beaad5d8bee27ec5dbfc3db3489396dde50f18a1
oveadee <a.szczechura.98@gmail.com>
2022-09-03 12:10:05 +0200

    feat: add 5 exercises

---
+++ exercise1/index.js
@@ -0,0 +1,6 @@
+//  create a function that gets 3 numbers as arguments and returns 1 which is multiply of them all
+
+// create a function that combines 2 strings
+
+// create a function that takes name: string as 1st argument and age as 2nd argument and print ie.
+// "Boby is 21 years old and he sucks"
---
+++ exercise3/index.js
@@ -0,0 +1,5 @@
+/* Stwórz projekt backendowy przy uyciu nodejs, biblioteki express i axios
+ * Projekt powinien odpalać się na porcie 3000
+ * Port powinien mieć jeden endpoint:
+ * GET na url 'https://insult.mattbas.org/api/insult'
+/* A zwrotka powinna być wyświetlana na ekranie po wejściu na root adres czyli '/'
